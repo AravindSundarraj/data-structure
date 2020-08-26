2c83188772ca3ebc367b3cb0c946488c7a57274c
Aravind Sundarraj <aravind.ssr@gmail.com>
2020-08-26 12:18:17 -0400

    binary search - iterative

---
+++ src/main/java/com/kaka/group/binarysearch/IterativeSearch.java
@@ -0,0 +1,35 @@
+package com.kaka.group.binarysearch;
+
+public class IterativeSearch {
+
+    public static void main(String args[]){
+        boolean flag =  findValue(new int[]{3 , 5 , 8, 10, 12, 15, 18,20,20,50,60} , 50);
+        System.out.println(flag);
+    }
+
+    public static boolean findValue(int a[] ,int value){
+
+        boolean flag = false;
+        int low = 0;
+        int high = a.length;
+     //   [3 , 5 , 8, 10, 12, 15, 18,20,20,50,60]
+        while(low <=high){
+         int mid =   (high - low) /2;
+         mid+=low;//11 - 6 = 5
+         if(a[mid] == value) {
+             return true;
+
+         }
+         else if(value < a[mid]){
+             high = mid - 1;
+         }
+         else if(value > a[mid]){
+            low = mid + 1; // 6
+         }
+
+        }
+
+        return flag;
+
+    }
+}
